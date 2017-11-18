351b8dcf0f96fc957021982d953d73ff03be2370
zdd <286814103@qq.com>
2017-11-18 22:21:51 +0800

    Update mini$.js

--- mini$.js
+++ mini$.js
@@ -1,3 +1,27 @@
+/*
+// 实现一个简单的Query
+function $(selector) {
+    
+}
+
+// 可以通过id获取DOM对象，通过#标示，例如
+$("#adom"); // 返回id为adom的DOM对象
+
+// 可以通过tagName获取DOM对象，例如
+$("a"); // 返回第一个<a>对象
+
+// 可以通过样式名称获取DOM对象，例如
+$(".classa"); // 返回第一个样式定义包含classa的对象
+
+// 可以通过attribute匹配获取DOM对象，例如
+$("[data-log]"); // 返回第一个包含属性data-log的对象
+
+$("[data-time=2015]"); // 返回第一个包含属性data-time且值为2015的对象
+
+// 可以通过简单的组合提高查询便利性，例如
+$("#adom .classa"); // 返回id为adom的DOM所包含的所有子节点中，第一个样式定义包含classa的对象
+*/
+
 function $(selector){
 	//这里定义ele是考虑到多个选择器组合的情况，ele初始值是document
 	var ele = document;
